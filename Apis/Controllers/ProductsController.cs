3d0641f1021cf8e8ec5c718aac81cc951ee07340
Aditya20042504 <AdityaDashrath.Dhaygude@cognizant.com>
2026-01-07 11:07:43 +0000

    Initial commit from public project

---
+++ Apis/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    // private static List<Product> products = new List<Product>
+    // {
+    //     new Product { ID = 1, Name = "Laptop", Price = 999.99f },
+    //     new Product { ID = 2, Name = "Smartphone", Price = 499.99f },
+    //     new Product { ID = 3, Name = "Tablet", Price = 299.99f }
+    // };
+    private AppDbContext appDbContext;
+    public ProductsController(AppDbContext appDbContext)
+    {
+        this.appDbContext = appDbContext;
+    }
+
+    [HttpGet]
+
+    [HttpGet("api/products")]
+    public ActionResult<IEnumerable<Product>> GetProducts()
+    {
+        return appDbContext.Products.ToList();
+    }
+
+    [HttpGet("api/products/{id}")]
+    // public ActionResult<Product> GetProduct(int id)
+    // {
+    //     var product = products.FirstOrDefault(p => p.ID == id);
+    //     if (product == null)
+    //     {
+    //         return NotFound();
+    //     }
+    //     return Ok(product);
+    // }
+
+    [HttpPost()]
+    public void Post([FromBody] Product newProduct)
+    {
+        appDbContext.Products.Add(newProduct);
+        appDbContext.SaveChanges();
+        return ;
+    }
+
+    // [HttpPut]
+    // public void Put(int id , Product product)
+    // {
+    //     var oldProduct = products.Find(p => p.ID == id);
+    //     oldProduct.Name = product.Name;
+    //     oldProduct.Price = oldProduct.Price;
+
+    // }
+
+    [HttpDelete]
+    public void Delete(int id)
+    {
+        var oldProduct = appDbContext.Products.Find(id);
+        appDbContext.Products.Remove(oldProduct);
+        appDbContext.SaveChanges();
+    }
+}
---
+++ FirstMVC/Models/Product.cs
@@ -0,0 +1,5 @@
+public class Product{
+    public int ID {get; set;}
+    public string Name {get; set;}
+    public float Price {get; set;}
+}
